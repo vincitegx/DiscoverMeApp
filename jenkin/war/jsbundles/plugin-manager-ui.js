80808eb707ec69ce56db3631a37242061dce974b
David Ogbodu <davidogbodu3056@gmail.com>
2023-09-17 19:28:53 +0100

    Refactored dev and prod profiles

--- jenkin/war/jsbundles/plugin-manager-ui.js
+++ jenkin/war/jsbundles/plugin-manager-ui.js
@@ -1,7 +1,7 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 7988:
+/***/ 4199:
 /***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -12,14 +12,14 @@ var debounce_default = /*#__PURE__*/__webpack_require__.n(debounce);
 // EXTERNAL MODULE: ./src/main/js/templates/plugin-manager/available.hbs
 var available = __webpack_require__(4746);
 var available_default = /*#__PURE__*/__webpack_require__.n(available);
-// EXTERNAL MODULE: ./.yarn/cache/jquery-npm-3.7.0-a02a382bf4-907785e133.zip/node_modules/jquery/dist/jquery.js
-var jquery = __webpack_require__(205);
+// EXTERNAL MODULE: ./.yarn/cache/jquery-npm-3.7.1-eeeac0f21e-4370b8139d.zip/node_modules/jquery/dist/jquery.js
+var jquery = __webpack_require__(4198);
 var jquery_default = /*#__PURE__*/__webpack_require__.n(jquery);
 // EXTERNAL MODULE: ./.yarn/cache/window-handle-npm-1.0.1-369b8e9cbe-8f2c183a0d.zip/node_modules/window-handle/index.js
 var window_handle = __webpack_require__(30);
-// EXTERNAL MODULE: ./.yarn/cache/handlebars-npm-4.7.7-a9ccfabf80-1e79a43f5e.zip/node_modules/handlebars/dist/cjs/handlebars.runtime.js
-var handlebars_runtime = __webpack_require__(9856);
-var handlebars_runtime_default = /*#__PURE__*/__webpack_require__.n(handlebars_runtime);
+// EXTERNAL MODULE: ./.yarn/cache/handlebars-npm-4.7.8-25244c2c82-00e68bb5c1.zip/node_modules/handlebars/runtime.js
+var runtime = __webpack_require__(4389);
+var runtime_default = /*#__PURE__*/__webpack_require__.n(runtime);
 ;// CONCATENATED MODULE: ./src/main/js/util/jenkins.js
 /**
  * Jenkins JS Modules common utility functions
@@ -151,7 +151,7 @@ jenkins.post = function (url, data, success, options) {
  *  handlebars setup, done for backwards compatibility because some plugins depend on it
  */
 jenkins.initHandlebars = function () {
-  return (handlebars_runtime_default());
+  return (runtime_default());
 };
 
 /**
@@ -637,6 +637,7 @@ function applyFilter(searchQuery) {
       admin
     });
     tbody.insertAdjacentHTML("beforeend", rows);
+    updateInstallButtonState();
   });
 }
 var handleFilter = function (e) {
@@ -660,13 +661,34 @@ document.addEventListener("DOMContentLoaded", function () {
     notificationBar.show(updateCenterError.content.textContent, notificationBar.ERROR);
   }
 });
+function updateInstallButtonState() {
+  // Enable/disable the 'Install' button depending on if any plugins are checked
+  const anyCheckboxesSelected = () => {
+    return document.querySelectorAll("input[type='checkbox']:checked").length > 0;
+  };
+  const installButton = document.querySelector("#button-install");
+  const installAfterRestartButton = document.querySelector("#button-install-after-restart");
+  if (!anyCheckboxesSelected()) {
+    installButton.disabled = true;
+    installAfterRestartButton.disabled = true;
+  }
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("click", () => {
+      setTimeout(() => {
+        installButton.disabled = !anyCheckboxesSelected();
+        installAfterRestartButton.disabled = !anyCheckboxesSelected();
+      });
+    });
+  });
+}
 
 /***/ }),
 
 /***/ 4746:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
-var Handlebars = __webpack_require__(2280);
+var Handlebars = __webpack_require__(4389);
 function __default(obj) { return obj && (obj.__esModule ? obj["default"] : obj); }
 module.exports = (Handlebars["default"] || Handlebars).template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -1013,7 +1035,7 @@ module.exports = (Handlebars["default"] || Handlebars).template({"1":function(co
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [216], function() { return __webpack_require__(7988); })
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [216], function() { return __webpack_require__(4199); })
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
