4ac44028b1430adfe5fb38e21889152c318f0873
David Tega <davidogbodu3056@gmail.com>
2023-09-18 16:51:47 +0100

    New Commit

---
+++ frontend/src/app/components/login-button/login-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-login-button',
+  templateUrl: './login-button.component.html'
+})
+export class LoginButtonComponent implements OnInit{
+  constructor(public auth: AuthService){}
+
+  ngOnInit(): void{}
+
+  loginWithRedirect():void{
+    this.auth.loginWithRedirect();
+  }
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  auth: {
+      domain: 'dev-us7x34wyw3ef5c4o.us.auth0.com',
+      clientId: 'dQLxtpUczr0VdNoDZJiTzYsvw3ANWEyU'
+    }
+};
